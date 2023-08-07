66eb5d345b54022e386d8f30b79d36ff22719a90
Andronov Ivan (Ber Tea) <andronoviv4@gmail.com>
2023-08-07 17:37:37 +0300

    Added 36 HW, but its so-so

--- 36th_HW.py
+++ 36th_HW.py
@@ -5,9 +5,24 @@
 # **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
 # **Вывод:**
 # 1 2 3 4 5 6
-
 # 2 4 6 8 10 12
 # 3 6 9 12 15 18
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+def print_operation_table(operation):
+    num_rows = 6
+    num_columns = 6
+
+    for i in range(1,num_columns+1):  
+        for j in range(1,num_rows+1):
+            if j == num_rows:
+                print (operation(i,j))
+            else: print (operation(i,j),end =" ")
+            
+
+
+print_operation_table(lambda x, y: x * y)            
+
+ #Стоун я не понимаю как я это сделал, но я сам, спасибо тебе за подсказки, и за стримчанский. Не пойму как через лист компрехейшн это написать. и вообще правильно ли я сделал?
